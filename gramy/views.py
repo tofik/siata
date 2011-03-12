65cad5eed1c946f836793425a1c75d0013b8a14c
Tomek Filipczuk <tomek.filipczuk@gmail.com>
2011-03-12 12:16:57 +0100

    Modifies views again (just playing with branches).

--- gramy/views.py
+++ gramy/views.py
@@ -57,11 +57,9 @@ def szczegoly(request, id):
 
         if granie.uczestnik_set.all().count() >= 2:
             message = '%s sie zapisal i jest komplet' % uczestnik.nick
-            print 'gramy'
-        # send_im_chat(message, 'tofikowy01@gmail.com')
+            send_im_chat(message, 'tofikowy01@gmail.com')
         if granie.uczestnik_set.all().count() == 0:
-            print 'nie ma chetnych'
-            # send_im_chat('nie gramy', 'tofikowy01@gmail.com')
+            send_im_chat('nie ma chetnych', 'tofikowy01@gmail.com')
 
     return render_to_response('gramy/szczegoly.html', {'granie': granie,
                                                        'uczestnicy': uczestnicy,
